55ae1835670014c0b320900cb2c8bac8675dd04f
Patrick Coleman <pcolemanofficial@gmail.com>
2021-01-04 19:58:28 -0500

    gunicorn

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ WORKDIR /sdc
 RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
 RUN pip install psycopg2
 RUN pip install -r requirements.txt
-ENTRYPOINT [ "python" ]
-CMD [ "run.py" ]
+RUN pip install gunicorn
+CMD [ "gunicorn", "--workers", "3", "--bind", "0.0.0.0:80", "run:app" ]
--- run.py
+++ run.py
@@ -2,12 +2,11 @@ from app import app
 from app.utils.conf_parse import get_config
 from app.utils.sql import sql_queries
 
-c = get_config()
-
-if c['SDC_SQL_DROP'] == 'true':
-    sql_queries.drop_database(c['SDC_SQL_DB'])
-    sql_queries.create_database(c['SDC_SQL_DB'])
-    sql_queries.create_fighter_table()
-    sql_queries.create_bout_table()
-
-app.run(debug=False, host=c['SDC_HOST'], port=c['SDC_PORT'])
+if __name__=='__main__':
+    c = get_config()
+    if c['SDC_SQL_DROP'] == 'true':
+        sql_queries.drop_database(c['SDC_SQL_DB'])
+        sql_queries.create_database(c['SDC_SQL_DB'])
+        sql_queries.create_fighter_table()
+        sql_queries.create_bout_table()
+    app.run(debug=False, host=c['SUI_HOST'], port=c['SUI_PORT'])
